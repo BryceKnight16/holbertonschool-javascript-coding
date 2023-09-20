91e71072feedb2ab9c1f9aade28f69d1be733d19
BryceKnight16 <6373@holbertonstudents.com>
2023-09-20 06:37:41 +0000

    files worked on live code

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const filePath = process.argv[2];
+const fileContent = process.argv[3];
+const fs = require('fs');
+
+fs.writeFile(filePath, fileContent, (err) => {
+  if (err) throw err;
+});
